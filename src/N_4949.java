2a72215176fb18883d5731796bf91e306c49767e
Lee Kaon <dlrkdhsoff@gmail.com>
2024-04-28 19:27:32 +0900

    .

---
+++ src/N_4949.java
@@ -0,0 +1,42 @@
+import java.io.*;
+import java.util.Scanner;
+import java.util.Stack;
+
+
+public class N_4949 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        while (true) {
+            String str = scan.nextLine();
+            if (".".equals(str)) {
+                break;
+            }
+            System.out.println(check(str));
+        }
+    }
+
+    public static String check(String str) {
+        Stack<Character> stack = new Stack<>();
+        for (char ch : str.toCharArray()) {
+            switch (ch) {
+                case '(':
+                case '[':
+                    stack.push(ch);
+                    break;
+                case ')':
+                    if (stack.isEmpty() || stack.pop() != '(') {
+                        return "no";
+                    }
+                    break;
+                case ']':
+                    if (stack.isEmpty() || stack.pop() != '[') {
+                        return "no";
+                    }
+                    break;
+            }
+        }
+        return stack.isEmpty() ? "yes" : "no";
+    }
+}

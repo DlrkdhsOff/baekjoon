80e4ee51c51a01172b28e3231a458b577b2e83e5
Lee Kaon <dlrkdhsoff@gmail.com>
2024-05-04 19:53:03 +0900

    .

---
+++ src/N_11866.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class N_11866 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder("<");
+        Queue<Integer> q = new LinkedList<>();
+
+        int nums = scan.nextInt();
+        int skip = scan.nextInt();
+
+        for (int i = 1; i <= nums; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+
+            for (int i = 0; i < skip -1; i++) {
+                q.offer(q.poll());
+            }
+            sb.append(q.poll()).append(", ");
+        }
+        sb.append(q.poll()).append(">");
+
+        System.out.println(sb);
+    }
+}

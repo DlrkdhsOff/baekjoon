d7b66ad3a57f22a0ff2128c6900e8f4c419222c3
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 07:31:40 +0900

    꼬마정민

---
+++ src/step_01/N_11382.java
@@ -0,0 +1,11 @@
+package step_01;
+
+import java.util.Scanner;
+
+public class N_11382 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println(scan.nextLong() + scan.nextLong() + scan.nextLong());
+    }
+}

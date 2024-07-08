e4a035889a75e88e27fcc07e12393de535782fe0
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 08:01:55 +0900

    시험 성적

---
+++ src/step_2/N_9798.java
@@ -0,0 +1,24 @@
+package step_2;
+
+import java.util.Scanner;
+
+public class N_9798 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int score = scan.nextInt();
+        scan.nextLine();
+
+        if (score >= 90) {
+            System.out.println("A");
+        } else if (score >= 80) {
+            System.out.println("B");
+        } else if (score >= 70) {
+            System.out.println("C");
+        } else if (score >= 60) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+    }
+}

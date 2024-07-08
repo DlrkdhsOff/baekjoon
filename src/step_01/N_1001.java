ea272288462468934effdf901854b4f34fc5ef07
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 06:51:22 +0900

    A - B

---
+++ src/step_01/N_1001.java
@@ -0,0 +1,15 @@
+package step_01;
+
+import java.util.*;
+
+public class N_1001 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println(A - B);
+    }
+}

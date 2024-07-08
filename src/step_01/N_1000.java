45c30d411bd5ad689c065fe216f8b0adbc3b0b0f
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 06:49:53 +0900

    A + B

---
+++ src/step_01/N_1000.java
@@ -0,0 +1,15 @@
+package step_01;
+
+import java.util.Scanner;
+
+public class N_1000 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println(A + B);
+    }
+}

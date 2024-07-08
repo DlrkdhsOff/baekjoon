1c7ec88a934b0bd441ab89c95ab4928b4913e56c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 08:55:37 +0900

    주사위 세개

---
+++ src/step_2/N_2480.java
@@ -0,0 +1,24 @@
+package step_2;
+
+import java.util.Scanner;
+
+public class N_2480 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+        scan.nextLine();
+
+        if (a == b && a == c) {
+            System.out.println(10000 + (a * 1000));
+        }else if (a == b || a == c || b == c){
+            int num = a == b ? a : c;
+            System.out.println(1000 + (num * 100));
+        } else {
+            int max = Math.max(Math.max(a, b), c);
+            System.out.println(max * 100);
+        }
+    }
+}

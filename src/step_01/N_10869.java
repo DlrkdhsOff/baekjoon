0777db973886cd6fd36f053ba584935dd2154e3b
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 06:58:07 +0900

    사칙연산

---
+++ src/step_01/N_10869.java
@@ -0,0 +1,19 @@
+package step_01;
+
+import java.util.Scanner;
+
+public class N_10869 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println(A + B);
+        System.out.println(A - B);
+        System.out.println(A * B);
+        System.out.println(A / B);
+        System.out.println(A % B);
+    }
+}

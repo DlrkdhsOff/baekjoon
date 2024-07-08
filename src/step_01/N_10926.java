a84c7e1a5731117b5c360cf1b5e7e93ba5d90d0e
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 07:02:24 +0900

    ??!

---
+++ src/step_01/N_10926.java
@@ -0,0 +1,12 @@
+package step_01;
+
+import java.util.Scanner;
+
+public class N_10926 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String str = scan.nextLine();
+        System.out.printf("%s%s", str, "??!");
+    }
+}

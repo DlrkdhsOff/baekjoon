684b2269146e07bde125b270d4a4e5673bdd2cd9
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 07:44:46 +0900

    고양이

---
+++ src/step_01/N_10171.java
@@ -0,0 +1,10 @@
+package step_01;
+
+public class N_10171 {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}

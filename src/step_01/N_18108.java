34fc3fac8cdc967204e1e0ad01e0548cc18ab8f2
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 07:13:07 +0900

    1998년생인 내가 태국에서는 2541년생?

---
+++ src/step_01/N_18108.java
@@ -0,0 +1,13 @@
+package step_01;
+
+import java.util.Scanner;
+
+public class N_18108 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int bc = scan.nextInt();
+
+        System.out.println(bc - 543);
+    }
+}

a49a8d29b82c1613f6c12309cd60c845c8c8f9ad
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 07:58:20 +0900

    두 수 비교하기

---
+++ src/step_2/N_1330.java
@@ -0,0 +1,21 @@
+package step_2;
+
+import java.util.Scanner;
+
+public class N_1330 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int A = scan.nextInt();
+        int B = scan.nextInt();
+        scan.nextLine();
+
+        if (A > B) {
+            System.out.println(">");
+        } else if (A < B) {
+            System.out.println("<");
+        } else if (A == B) {
+            System.out.println("==");
+        }
+    }
+}

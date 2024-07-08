8715cd29fe30573413c397afd2cb45e57095e463
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 06:54:52 +0900

    A / B

---
+++ src/step_01/N_1008.java
@@ -0,0 +1,15 @@
+package step_01;
+
+import java.util.Scanner;
+
+public class N_1008 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        double A = scan.nextInt();
+        double B = scan.nextInt();
+        scan.nextLine();
+
+        System.out.println(A / B);
+    }
+}

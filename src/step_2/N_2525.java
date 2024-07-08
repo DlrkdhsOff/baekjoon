af29e321816c0f2b854dfcad3f60a71c1111c790
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 08:37:31 +0900

    오븐 시계

---
+++ src/step_2/N_2525.java
@@ -0,0 +1,23 @@
+package step_2;
+
+import java.util.Scanner;
+
+public class N_2525 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int hour = scan.nextInt();
+        int min = scan.nextInt();
+        int end = scan.nextInt();
+        scan.nextLine();
+
+        hour += (min + end) / 60;
+        min = (min + end) % 60;
+
+        if (hour >= 24) {
+            hour -= 24;
+        }
+
+        System.out.printf("%d %d", hour, min);
+    }
+}

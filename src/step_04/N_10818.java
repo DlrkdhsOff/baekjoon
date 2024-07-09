ea1f4c434b952c15bb63a0cbd3b2aed40f1edf7c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 10:39:01 +0900

    최소, 최대

---
+++ src/step_04/N_10818.java
@@ -0,0 +1,21 @@
+package step_04;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Scanner;
+
+public class N_10818 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int start = scan.nextInt();
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < start; i++) {
+            list.add(scan.nextInt());
+        }
+        list.sort(Comparator.naturalOrder());
+        System.out.printf("%d %d", list.getFirst(), list.getLast());
+    }
+}

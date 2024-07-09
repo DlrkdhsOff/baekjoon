9fa2dc0298ba94fedbe181154ab79116f90c88f2
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-10 01:08:18 +0900

    문자와 문자열

---
+++ src/step_05/N_27866.java
@@ -0,0 +1,13 @@
+package step_05;
+
+import java.util.Scanner;
+
+public class N_27866 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.nextLine();
+
+        System.out.println(s.charAt(scan.nextInt() - 1));
+    }
+}

4d40ad6d6cd3fc7c855538f2bdd8047c0ce56e34
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 09:06:48 +0900

    윤년

--- src/step_02/N_2753.java
+++ src/step_02/N_2753.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class N_2753 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
         int year = scan.nextInt();
         scan.nextLine();
 

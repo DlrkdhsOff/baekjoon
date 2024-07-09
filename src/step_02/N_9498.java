58f4fd669bc2c31b666deab8664fdcbb14232fe7
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-09 09:06:09 +0900

    시험 성적

--- src/step_02/N_9498.java
+++ src/step_02/N_9498.java
@@ -2,7 +2,7 @@ package step_02;
 
 import java.util.Scanner;
 
-public class N_9798 {
+public class N_9498 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 

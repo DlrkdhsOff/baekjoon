e1387e2f8d46ecf653923f3457cc34f9a4f300e3
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-10 01:09:22 +0900

    단어 길이 재기

---
+++ src/step_05/N_2743.java
@@ -0,0 +1,13 @@
+package step_05;
+
+import java.util.Scanner;
+
+public class N_2743 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        String s = scan.nextLine();
+
+        System.out.println(s.length());
+    }
+}

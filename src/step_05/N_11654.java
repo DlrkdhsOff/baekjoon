699dbd41732fb9c700615691a27e456ac8bddf36
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-10 01:23:32 +0900

    아스키 코드

---
+++ src/step_05/N_11654.java
@@ -0,0 +1,13 @@
+package step_05;
+
+import java.io.*;
+
+public class N_11654 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+
+        System.out.println((int)s.charAt(0));
+    }
+}

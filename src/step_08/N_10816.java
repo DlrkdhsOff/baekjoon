40b224bac6487b434d27696825f189820bc0f536
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-11 08:36:57 +0900

    숫자 카드 2

---
+++ src/step_08/N_10816.java
@@ -0,0 +1,32 @@
+package step_08;
+
+import java.io.*;
+import java.util.*;
+
+public class N_10816 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        Map<String, Integer> map = new HashMap<>();
+        String[] card = br.readLine().split(" ");
+
+        for (String s : card) {
+            map.put(s, map.getOrDefault(s,0) + 1);
+        }
+
+        int M = Integer.parseInt(br.readLine());
+        String[] hold = br.readLine().split(" ");
+
+        for (String s : hold) {
+            if (map.containsKey(s)) {
+                bw.write(map.get(s) + " ");
+            } else {
+                bw.write(0 + " ");
+            }
+        }
+        bw.flush();
+
+    }
+}

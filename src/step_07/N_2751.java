36fcb4103363715f88c80de3440603aacdba9659
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-10 09:59:47 +0900

    수 정렬하기 2

---
+++ src/step_07/N_2751.java
@@ -0,0 +1,27 @@
+package step_07;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class N_2751 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int loop = Integer.parseInt(br.readLine());
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < loop; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+
+        for (int num : list) {
+            bw.write(num + "\n");
+        }
+        bw.flush();
+
+    }
+}

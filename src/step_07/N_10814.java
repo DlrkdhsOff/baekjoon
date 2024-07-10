36e27ca708bc9c44c7de427d99f28bbc787985fb
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-10 12:50:42 +0900

    나이순 정렬

---
+++ src/step_07/N_10814.java
@@ -0,0 +1,33 @@
+package step_07;
+
+import java.util.*;
+
+public class N_10814 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int N = scan.nextInt();
+        scan.nextLine();
+
+        List<String[]> list = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            list.add(scan.nextLine().split(" "));
+        }
+
+        list.sort(new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                if (Integer.parseInt(o1[0]) == Integer.parseInt(o2[0])) {
+                    return Integer.compare(Integer.parseInt(o1[0]), Integer.parseInt(o2[0]));
+                } else {
+                    return Integer.parseInt(o1[0]) - Integer.parseInt(o2[0]);
+                }
+            }
+        });
+
+        for (String[] s : list) {
+            System.out.println(s[0] + " " + s[1]);
+        }
+    }
+}

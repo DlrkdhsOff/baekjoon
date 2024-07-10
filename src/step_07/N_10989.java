dac3ddf240a72e74d4472e9126439a5942299f5b
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-10 10:16:00 +0900

    수 정렬하기 3

---
+++ src/step_07/N_10989.java
@@ -0,0 +1,25 @@
+package step_07;
+
+import java.io.*;
+import java.util.Arrays;
+
+public class N_10989 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int loop = Integer.parseInt(br.readLine());
+        int[] arr = new int[loop];
+
+        for (int i = 0; i < loop; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        Arrays.sort(arr);
+
+        for (int num : arr) {
+            bw.write(num + "\n");
+        }
+        bw.flush();
+
+    }
+}

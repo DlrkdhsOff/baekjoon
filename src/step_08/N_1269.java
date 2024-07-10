b911ba98ebcdbe699ec90cecc20eb4688a060c9f
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-11 08:52:39 +0900

    대칭 차집합

---
+++ src/step_08/N_1269.java
@@ -0,0 +1,30 @@
+package step_08;
+
+import java.io.*;
+import java.util.*;
+
+public class N_1269 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String index = br.readLine();
+
+        Set<String> A = new HashSet<>(Arrays.asList(br.readLine().split(" ")));
+        Set<String> B = new HashSet<>(Arrays.asList(br.readLine().split(" ")));
+
+        int count = 0;
+        for (String s : A) {
+            if (!B.contains(s)) {
+                count++;
+            }
+        }
+        for (String s : B) {
+            if (!A.contains(s)) {
+                count++;
+            }
+        }
+        bw.write(count + "\n");
+        bw.flush();
+    }
+}

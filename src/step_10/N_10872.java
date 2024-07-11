2b6d8d880336c35c379f14e0eaea88c42cba0c1c
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-12 08:48:00 +0900

    팩토리얼

---
+++ src/step_10/N_10872.java
@@ -0,0 +1,22 @@
+package step_10;
+
+import java.io.*;
+
+public class N_10872 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+
+        bw.write(fact(n, 1) + "\n");
+        bw.flush();
+    }
+
+    public static int fact(int n, int result) {
+        if (n < 1) {
+            return result;
+        }
+        return fact(n - 1, result * n);
+    }
+}

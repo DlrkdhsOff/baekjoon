f4f786dd65f5acfce5c6855b50e9d9e37df7f02d
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-12 08:35:38 +0900

    베라의 패션

---
+++ src/step_10/N_15439.java
@@ -0,0 +1,15 @@
+package step_10;
+
+import java.io.*;
+
+public class N_15439 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(br.readLine());
+        int result = n * (n - 1);
+        bw.write(result + "\n");
+        bw.flush();
+    }
+}
---
+++ src/step_10/N_24723.java
@@ -0,0 +1,15 @@
+//package step_10;
+//
+//import java.io.*;
+//
+//public class N_24723 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int n = Integer.parseInt(br.readLine());
+//        int result = (int)Math.pow(2, n);
+//        bw.write(result + "\n");
+//        bw.flush();
+//    }
+//}

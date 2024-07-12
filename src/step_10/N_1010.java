c355f907337cf23ca317038821ce5989d184df19
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-12 09:33:30 +0900

    다리 놓기

---
+++ src/step_10/N_1010.java
@@ -0,0 +1,36 @@
+package step_10;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class N_1010 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine()); // 테스트 케이스 개수
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            long result = combination(M, N);
+
+            bw.write(result + "\n");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    public static long combination(int n, int r) {
+        if (r > n / 2) {
+            r = n - r;  // C(n, r) == C(n, n-r)
+        }
+        long result = 1;
+        for (int i = 0; i < r; i++) {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
+    }
+}

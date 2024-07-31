cc499050fb80a522eedbc83f5680f859f547a35b
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-07-31 21:24:41 +0900

    크로아티아 알파벳

---
+++ src/step_19/N_24060.java
@@ -0,0 +1,28 @@
+//package step_19;
+//
+//import java.io.*;
+//import java.util.StringTokenizer;
+//
+//public class N_24060 {
+//    public static void main(String[] args) throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        String s = br.readLine();
+//        StringTokenizer token = new StringTokenizer(s);
+//
+//        int index = Integer.parseInt(token.nextToken());
+//        int save = Integer.parseInt(token.nextToken());
+//
+//        int[] data = new int[index];
+//
+//        String[] strings = br.readLine().split(" ");
+//        for (int i = 0; i < index; i++) {
+//            data[i] = Integer.parseInt(strings[i]);
+//        }
+//
+//
+//    }
+//}
+////5 7
+////4 5 1 3 2

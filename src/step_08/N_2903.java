9d0a366ecc3f545191d14c1a5be6b0f344f6ebd4
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-08-06 16:08:18 +0900

    중앙 이동 알고리즘

--- src/step_08/N_2903.java
+++ src/step_08/N_2903.java
@@ -2,7 +2,7 @@ package step_08;
 
 import java.io.*;
 
-public class Main {
+public class N_2903 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));

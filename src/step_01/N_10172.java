380b333e2e8b1b25b5790c91d8a1422e5a785043
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-08-07 16:34:48 +0900

    개

--- src/step_01/N_10172.java
+++ src/step_01/N_10172.java
@@ -2,11 +2,7 @@ package step_01;
 
 public class N_10172 {
     public static void main(String[] args) {
-        System.out.println("|\\_/|");
-        System.out.println("|q p|   /}");
-        System.out.println("( 0 )\"\"\"\\");
-        System.out.println("|\"^\"`    |");
-        System.out.println("||_/=\\\\__|");
+        System.out.println("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`    |\n||_/=\\\\__|");
 
     }
 }

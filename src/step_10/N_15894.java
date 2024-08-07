628494fe4e14f735b48fd05290492ec5c0114933
이가온 <165661849+DlrkdhsOff@users.noreply.github.com>
2024-08-07 16:22:58 +0900

    수학은 체육 과목입니다

---
+++ src/step_10/N_15894.java
@@ -0,0 +1,15 @@
+package step_10;
+
+import java.io.*;
+
+public class N_15894 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long num = Long.parseLong(br.readLine());
+
+        bw.write(num * 4 + "\n");
+        bw.flush();
+    }
+}
